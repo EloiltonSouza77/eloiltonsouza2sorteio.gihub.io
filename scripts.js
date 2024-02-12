570d08fb174907e417a67d6cb18fdd6d831475c8
eloiltonsouza77 <153831703+EloiltonSouza77@users.noreply.github.com>
2024-02-12 11:06:42 -0300

    1º Atualização

--- scripts.js
+++ scripts.js
@@ -6,8 +6,25 @@ const drawn = document.querySelector(".sort")
 
 
 
+
+
 const result = Math.floor(Math.random() * (inpNumber2 - inpNumber + 1)) + inpNumber;
 
 drawn.innerHTML = result
+
+
+
+if (inpNumber >= inpNumber2) {
+
+    alert ("O 1º número não pode ser maior ou igual ao 2º número")
+}
+
+if (inpNumber < 0){
+
+alert ("Os numeros não podem ser negativos")
+
+}
+
+
 
 }
